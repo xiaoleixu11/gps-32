73388708d63510dbf60c606d1da93acde87007a8
xiaoleixu11 <306784242plmplm@gmail.com>
2024-12-20 13:14:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,14 +8,17 @@ class TestStringMethods(unittest.TestCase):
     """Class representing something"""
 
     def test_is_upper(self):
+        """Function test_is_upper."""
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
+        """Function test_is_lower."""
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
+        """Function test_is_capitalize."""
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
