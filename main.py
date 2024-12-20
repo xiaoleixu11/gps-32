bd94651c16a6f5461a10371c93f6ae5b0bc68432
xiaoleixu11 <306784242plmplm@gmail.com>
2024-12-20 13:02:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-""" prova de CI/CD"""
 import transform
 
 
+# Docstring del módulo
+"""
+Este módulo proporciona una interfaz para que el usuario introduzca una cadena 
+y seleccione una transformación para convertirla a mayúsculas, minúsculas o capitalizarla.
+Utiliza funciones del módulo 'transform' para realizar las transformaciones.
+"""
+
 def main():
-    """ core del la prova"""
-    string = input("Introdueix un string:")
+    """Función principal que gestiona la entrada del usuario y realiza 
+    las transformaciones de texto según la opción seleccionada.
+    """
+    string = input("Introdueix un string: ")
 
     print("Quina transformació vols?")
-    print("[1] Text amb tot majuscules")
+    print("[1] Text amb tot majúscules")
     print("[2] Text amb tot minúscuies")
     print("[3] Text capitalitzat ")
 
@@ -20,7 +28,7 @@ def main():
     elif opcio == "3":
         print(transform.to_capitalize(string))
     else:
-        print("opció no reconegudda")
+        print("opció no reconeguda")
 
 
 if __name__ == '__main__':
