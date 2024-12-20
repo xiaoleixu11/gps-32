5ca1ecadf5798db655cab81ac1547268e377375e
xiaoleixu11 <306784242plmplm@gmail.com>
2024-12-20 12:55:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
 import unittest
 import transform
 
-
-# Docstring para el módulo
-"""
-Este módulo contiene pruebas unitarias para verificar el funcionamiento 
-de las funciones de transformación de cadenas definidas en el módulo 
-'transform'.
-"""
-
 class TestStringMethods(unittest.TestCase):
-    """
-    Clase que contiene pruebas unitarias para las funciones de transformación
-    de cadenas, como convertir a mayúsculas, minúsculas y capitalizar.
-    """
 
     def test_is_upper(self):
         """
@@ -24,21 +12,14 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
-        """
-        Verifica que la función 'to_lower_case' convierta correctamente 
-        una cadena a minúsculas.
-        """
+
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
-        """
-        Verifica que la función 'to_capitalize' convierta correctamente 
-        la primera letra de una cadena a mayúsculas.
-        """
+        
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
-
 
 if __name__ == '__main__':
     unittest.main()
